8ac381a381e9fed772fe7a3c246473b6bde3a6b5
Irshad Ahmad Sheikh <irshad.mike@gmail.com>
2020-07-29 07:55:42 +0530

    Delete brainTree.config.ts
    hardcoded secret key present. have to remove it

--- src/payment/brainTree.config.ts
+++
@@ -1,8 +0,0 @@
-const braintree = require('braintree');
-
-export const gateway = braintree.connect({
-    environment: braintree.Environment.Sandbox,
-    merchantId: "m7gfvmm4drz9z6cf",
-    publicKey: "46xpv8t2dkr3hfbv",
-    privateKey: "64faa5d353b89b156469da88046cb331"
-  });
